195d7e7c4df186d88ac48dea9406bd4eb6f551fb
Hassan <Mallik.Hassan@gnb.ca>
2020-08-18 16:33:37 -0300

    React intro

---
+++ scrimba-react-intro/src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MyInfo(){
+    return (
+      <div>
+        <h1>Hi, this is Sadi!</h1>
+        <ul>
+          <li>I want to go to Bangladesh</li>
+          <li>Any time, whenever I want</li>
+          <li>At least once, before I die!</li>
+        </ul>
+      </div>
+    );
+  }
+  
+export default MyInfo
