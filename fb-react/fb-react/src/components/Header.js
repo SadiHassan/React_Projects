e1b99f4f8fe9e465b6d803eaf1d5807eb2af3a14
sadi.research@gmail.com <sadi.research@gmail.com>
2020-11-07 00:41:16 -0400

    sn init

---
+++ fb-react/fb-react/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt="Facebook Logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
