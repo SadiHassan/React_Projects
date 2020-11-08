2036b04de6bbc0e781c1e2d51ed6fb382228821c
sadi.research@gmail.com <sadi.research@gmail.com>
2020-11-08 01:37:46 -0400

    fb-clone...

--- fb-react/fb-react/src/App.js
+++ fb-react/fb-react/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Header from './components/Header';
-
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
 function App() {
   return (
-    <div className="App">
-      <Header />
-      { /* Side bar */ }
-      { /* Feed */ }
-      { /* Widgets */ }
+    <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          { /* Widgets */ }
+        </div>
+        
     </div>
   );
 }
