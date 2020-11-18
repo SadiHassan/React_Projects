8bcb66b2f0a0fa2e586457ae11cc42791247ee02
Hassan <Mallik.Hassan@gnb.ca>
2020-11-18 14:28:23 -0400

    slack

---
+++ slack-clone/slack-clone/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM Naming Convention
+    <div className="App">
+      <h1>Hello Friends :rock </h1>
+
+      <Header />
+      {/* Side bar */}
+      {/* React Router --> Chat Screen */}
+
+    </div>
+  );
+}
+
+export default App;
