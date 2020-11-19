0e307b50818a7465dacc1e9166bcf464d91160c8
sadi.research@gmail.com <sadi.research@gmail.com>
2020-11-19 01:05:11 -0400

    sss

--- slack-clone/slack-clone/src/App.js
+++ slack-clone/slack-clone/src/App.js
@@ -1,14 +1,15 @@
-
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     // BEM Naming Convention
     <div className="App">
-      <h1>Hello Friends :rock </h1>
-
       <Header />
+      <div className = "app__body">
+        <Sidebar />
+      </div>
       {/* Side bar */}
       {/* React Router --> Chat Screen */}
 
