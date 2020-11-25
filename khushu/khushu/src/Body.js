2ba6bf9fe5d5ee73319d06419c617f383f824209
Hassan <Mallik.Hassan@gnb.ca>
2020-11-25 16:58:45 -0400

    khushu++

---
+++ khushu/khushu/src/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className = "body">
+            I am body
+        </div>
+    )
+}
+
+export default Body
