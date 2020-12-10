d5c084ff3b3fa0a561254095b607e5d319d51538
sadi.research@gmail.com <sadi.research@gmail.com>
2020-12-10 00:33:08 -0400

    khushu updates

--- khushu/khushu/src/Books.js
+++ khushu/khushu/src/Books.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './Header'
 
 function Books() {
     return (
         <div>
-            Books
+            <Header />
+            
         </div>
     )
 }
